4f008537a789c35a6d7601c93d3a6ca8c8a3d225
Nick Matelli <nmatelli@enova.com>
2018-09-03 14:23:05 -0500

    Create UI for search/landing screen

---
+++ Fairental/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Fairental
+//
+//  Created by Nick Matelli on 9/3/18.
+//  Copyright © 2018 Foo. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
