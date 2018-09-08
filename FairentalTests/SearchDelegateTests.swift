2cdbb8870e1c795f2ff21467b5799389d31c3181
Nick Matelli <nmatelli@enova.com>
2018-09-08 16:41:33 -0500

    Added a test to help check on parsing logic

--- FairentalTests/SearchDelegateTests.swift
+++ FairentalTests/SearchDelegateTests.swift
@@ -6,5 +6,31 @@
 //  Copyright © 2018 Foo. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import Fairental
+
+class SearchDelegateTests: XCTestCase {
+    var target: SearchDelegate!
+    
+    override func setUp() {
+        super.setUp()
+        target = SearchDelegate(SearchViewModel())
+    }
+    
+    func testParseResults() {
+        let data = loadJSON("SearchResponse")
+        XCTAssertNotNil(data)
+        let parsedData = target.decodeData(data)
+        XCTAssertEqual("XXAR", parsedData?.results.first?.cars.first?.vehicleInfo.acrissCode)
+    }
+    
+    func loadJSON(_ fileName: String) -> Data? {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: "json"),
+            let data = try? Data(contentsOf: url) {
+            return data
+        }
+        return nil
+    }
+}
+
 
