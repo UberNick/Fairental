b715ae5fd8c2e1f04d7edc4df8c068042cbba676
Nick Matelli <nmatelli@enova.com>
2018-09-09 13:46:42 -0500

    Updated GPS icon to use button color

---
+++ Fairental/Controller/DirectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DirectionViewController.swift
+//  Fairental
+//
+//  Created by Nick Matelli on 9/9/18.
+//  Copyright © 2018 Foo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DirectionViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        print("Hello, world")
+    }
+}
